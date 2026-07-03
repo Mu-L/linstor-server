2910dd442355ffb2caa7e1a8622db0f2ceacc085
Rene Peinthor <rene.peinthor@linbit.com>
2026-07-03 12:01:39 +0200

    Remove security-related debug console commands

--- server/src/main/java/com/linbit/linstor/debug/CmdShutdown.java
+++ server/src/main/java/com/linbit/linstor/debug/CmdShutdown.java
@@ -42,7 +42,7 @@ public class CmdShutdown extends BaseDebugCmd
         Map<String, String> parameters
     ) throws Exception
     {
-        applicationLifecycleManager.shutdown(accCtx);
+        applicationLifecycleManager.shutdown();
         debugCon.exitConsole();
     }
 }
